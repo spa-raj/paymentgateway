8595182060436cbb01ab256639eb665873a7e2f0
Sparsh <sparsh.raj30@gmail.com>
2025-11-08 21:59:52 +0530

    Initial commit: Spring Boot payment gateway project setup
      Set up basic Spring Boot project structure for payment gateway application.
    
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/paymentgateway/PaymentgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vibevault.paymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentgatewayApplication.class, args);
+    }
+
+}
