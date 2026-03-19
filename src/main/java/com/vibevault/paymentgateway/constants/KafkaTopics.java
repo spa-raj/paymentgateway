4d8a1fe1a68e2a805bd8a82a61e6e47a19fe2955
Sparsh <sparsh.raj30@gmail.com>
2026-03-19 21:04:29 +0530

    Add KafkaTopics constants for order-events and payment-events
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/paymentgateway/constants/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.vibevault.paymentgateway.constants;
+
+public final class KafkaTopics {
+    public static final String ORDER_EVENTS = "order-events";
+    public static final String PAYMENT_EVENTS = "payment-events";
+
+    private KafkaTopics() {
+    }
+}
