732c98f8533a42c68a3ea094647da04097a22490
Sparsh <sparsh.raj30@gmail.com>
2026-03-19 20:38:53 +0530

    Add @Validated + @NotBlank on Razorpay keys for fail-fast startup
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- src/main/java/com/vibevault/paymentgateway/configurations/RazorpayConfig.java
+++ src/main/java/com/vibevault/paymentgateway/configurations/RazorpayConfig.java
@@ -2,20 +2,27 @@ package com.vibevault.paymentgateway.configurations;
 
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "razorpay")
 public class RazorpayConfig {
 
+    @NotBlank(message = "razorpay.key-id must be configured")
     private String keyId;
+
+    @NotBlank(message = "razorpay.key-secret must be configured")
     private String keySecret;
+
     private String webhookSecret;
 
     @Bean
