965fc522db18df8feb5223296b5629462ae6caf9
Sparsh <sparsh.raj30@gmail.com>
2026-03-19 21:42:24 +0530

    Handle MissingRequestHeader and MethodNotAllowed explicitly
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- src/main/java/com/vibevault/paymentgateway/advices/ExceptionAdvices.java
+++ src/main/java/com/vibevault/paymentgateway/advices/ExceptionAdvices.java
@@ -9,7 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.owasp.encoder.Encode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -54,6 +56,16 @@ public class ExceptionAdvices {
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ExceptionDto> handleNoResourceFound(NoResourceFoundException ex, HttpServletRequest request) {
         return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request.getRequestURI(), "NOT_FOUND");
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ExceptionDto> handleMissingHeader(MissingRequestHeaderException ex, HttpServletRequest request) {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Missing header: " + ex.getHeaderName(), request.getRequestURI(), "BAD_REQUEST");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionDto> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
+        return buildResponse(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage(), request.getRequestURI(), "METHOD_NOT_ALLOWED");
     }
 
     @ExceptionHandler(org.springframework.dao.OptimisticLockingFailureException.class)
