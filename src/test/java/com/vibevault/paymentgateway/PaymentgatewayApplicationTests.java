13c14986faef507aac4ef771d6ac96656573436e
Sparsh <sparsh.raj30@gmail.com>
2026-03-19 18:08:18 +0530

    Upgrade to Spring Boot 4.0.3, Java 21 with all dependencies
      - pom.xml: JPA, Kafka, OAuth2, Flyway, MySQL, Razorpay SDK, OWASP encoder
      - application.properties: MySQL, Kafka, OAuth2, Flyway, Actuator, Razorpay config
      - Port 8084, paymentgateway consumer group
      - .dockerignore added
      - Removed old skeleton service/controller (will rebuild in later issues)
      - Maven surefire with byte-buddy agent for Mockito
    
      Closes #1
    
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- src/test/java/com/vibevault/paymentgateway/PaymentgatewayApplicationTests.java
+++ src/test/java/com/vibevault/paymentgateway/PaymentgatewayApplicationTests.java
@@ -1,9 +1,7 @@
 package com.vibevault.paymentgateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PaymentgatewayApplicationTests {
 
     @Test
